ebc62db57917785e53f5ffda84a5295989e6a035
c.morais <c.morais1507@gmail.com>
2022-10-18 23:46:36 -0300

    chore: Pasta DropDownMenu movida de diretório

---
+++ src/components/DropDownMenu/index.jsx
@@ -0,0 +1,8 @@
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+
+export function HeaderDropDown () {
+    <DropdownMenu.Root>
+        <DropdownMenu.Trigger />
+        
+    </DropdownMenu.Root>
+}
